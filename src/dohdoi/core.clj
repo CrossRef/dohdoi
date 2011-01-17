1ca12ad33e26799d28f6687c6a9798ba62bb8d44
Karl Jonathan Ward <karl.j.ward@gmail.com>
2011-01-17 10:11:36 +0000

    core.clj: write-doi-seq function that will write any doi sequence.

--- src/dohdoi/core.clj
+++ src/dohdoi/core.clj
@@ -26,18 +26,21 @@
   (let [file-list (map #(file d %) (.list (file d)))]
     (mapcat dois-in-file file-list)))
 
-(defn write-dois-from-dir
+(defn write-doi-seq
   "Writes dois from deposit dump files in a dir to a file."
-  [prefix in-dir out-file]
-  (let [dois (dois-in-dir in-dir)]
-    (loop [first-lot (take 1000 dois)
-	   rest-lot  (drop 1000 dois)]
-      (let [converted-dois (map #(str prefix %) first-lot)
-	    next-rest-lot (drop 1000 rest-lot)]
-	(spit out-file
-	      (join \newline converted-dois)
-	      :append true)
-	(if (seq next-rest-lot)
-	  (recur (take 1000 rest-lot) next-rest-lot))))))
+  [prefix doi-seq out-file]
+  (loop [first-lot (take 1000 doi-seq)
+	 rest-lot  doi-seq]
+    (let [converted-dois (map #(str prefix %) first-lot)]
+      (spit out-file
+	    (join \newline converted-dois)
+	    :append true)
+      (if (seq rest-lot)
+	(recur (take 1000 rest-lot) (drop 1000 rest-lot))))))
 
 ; e.g. (take 5000 (dois-in-dir "/Users/karl/Data/conf"))
+
+; or
+; (write-doi-seq "http://ip-10-48-71-165.eu-west-1.compute.internal/"
+; 		 (take 100000 (dois-in-dir "/Users/karl/Data/conf"))
+;		 "/Users/karl/Data/100000.conf.urls.txt")
