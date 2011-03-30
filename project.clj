999b3d4ecf65da213872b1443399e3101371ccf5
Karl Jonathan Ward <karl.j.ward@gmail.com>
2011-03-30 10:54:04 +0100

    project.clj: Missed a dependency.

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject dohdoi "0.0.1"
   :description "Tools for parsing CrossRef metadata deposit dump files."
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
+                 [org.clojure/clojure-contrib "1.2.0"]
+		 [congomongo "0.1.3-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.2.1"]]
   :jvm-opts ["-Xmx1g"])
 
